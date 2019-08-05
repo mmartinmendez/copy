9745137b5141957025f80cc886aa466b24d7de7e
Mithun Martin Mendez <mithun.martin.215@gmail.com>
2019-08-06 01:00:21 +0200

    template for useful links

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -25,7 +25,7 @@ const Contact = () => (
           <p>Raamstraat 8</p>
           <p>Delft</p>
         </div>
-        <a href="https://www.google.com/maps/place/Raamstraat+8,+2613+SC+Delft/@52.0065548,4.3528904,17z/data=!3m1!4b1!4m5!3m4!1s0x47c5b5c0cc59782d:0x20c7730917f5d3cc!8m2!3d52.0065515!4d4.3550791" target="__blank" style={{
+        <a href="https://www.google.com/maps/place/Raamstraat+8,+2613+SC+Delft/@52.0065548,4.3528904,17z/data=!3m1!4b1!4m5!3m4!1s0x47c5b5c0cc59782d:0x20c7730917f5d3cc!8m2!3d52.0065515!4d4.3550791" target="__blank" alt="raamstraat 8" style={{
             backgroundImage: `url(${raamstraat})`,
             backgroundPosition: "center",
             backgroundSize: "cover",
@@ -33,13 +33,13 @@ const Contact = () => (
             width: "100%",
             maxWidth: "800px",
             height: "300px"
-        }} />
+        }}>.</a>
         <div className="contact-address">
           <p>The activities that we have are normally at the following address:</p>
           <p>Voorstraat 60</p>
           <p>Delft</p>
         </div>
-        <a href="https://www.google.com/maps/place/Voorstraat+60,+2611+JS+Delft/@52.0146124,4.3532954,17z/data=!3m1!4b1!4m5!3m4!1s0x47c5b5c4a50631f1:0xba3a17421f31b0b4!8m2!3d52.0146091!4d4.3554841" target="__blank" style={{
+        <a href="https://www.google.com/maps/place/Voorstraat+60,+2611+JS+Delft/@52.0146124,4.3532954,17z/data=!3m1!4b1!4m5!3m4!1s0x47c5b5c4a50631f1:0xba3a17421f31b0b4!8m2!3d52.0146091!4d4.3554841" target="__blank" alt="voorstraat 60" style={{
             backgroundImage: `url(${voorstraat})`,
             backgroundPosition: "center",
             backgroundSize: "cover",
@@ -47,7 +47,7 @@ const Contact = () => (
             width: "100%",
             maxWidth: "800px",
             height: "300px"
-        }} />
+        }}>.</a>
       </div>
     </div>
   </Layout>
---
+++ src/pages/usefullinks/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Layout from '../../components/Layout'
+// import { graphql } from 'gatsby'
+
+const UsefulLinks = ({ data }) => (
+    <div>content</div>
+)
+
+export default UsefulLinks
+
+// export const UsefullinksPageQuery = graphql`
+//     query UsefulLinksQuery {
+
+//     }
+// `
