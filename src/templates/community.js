236f88582c957893324b588f700451e8db34abb6
Mithun Martin Mendez <mithun.martin.215@gmail.com>
2019-08-05 16:30:53 +0200

    adding community page

--- src/templates/community.js
+++ src/templates/community.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import Layout from '../components//Layout'
 
-const Community = () => (
-    <Layout>Community</Layout>
+const Community = ({ data }) => (
+    <Layout>
+        <p>{data.markdownRemark.frontmatter.title}</p>
+        <p>{data.markdownRemark.frontmatter.info}</p>
+    </Layout>
 )
 
 export default Community
+
+export const communityPageQuery = graphql`
+  query CommunityPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+        info
+      }
+    }
+  }
+`
