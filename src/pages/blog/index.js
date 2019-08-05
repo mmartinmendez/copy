39e62bcf523c6912687826a50a458bdbf1f1ab71
Mithun Martin Mendez <mithun.martin.215@gmail.com>
2019-08-05 16:38:00 +0200

    adding blog page

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,17 +1,20 @@
 import React from 'react'
 
 import Layout from '../../components/Layout'
+import Content from '../../components/Content'
+
+import './blog.sass'
+
 
 export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-            </div>
-          </div>
-        </section>
+        <div className="container blog-description">
+          <p>Our community is ever changing. There are always fresh faces. The newcomers in Delft mostly need information on using their time more efficiently and access to critical information. We provide some information here so that your stay is a little bit more comfortable.</p>
+
+          <p style={{paddingBottom: "700px"}}>If you want to contribute towards growing this resource page <a href="#url">contact us</a>. </p>
+        </div>
       </Layout>
     )
   }
