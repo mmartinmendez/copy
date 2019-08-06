9e12d60a6f767ad0880c44d320ada18560d0f309
Mithun Martin Mendez <mithun.martin.215@gmail.com>
2019-08-06 22:40:29 +0200

    activities page added

---
+++ src/templates/activity.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Activity = () => (
+  <div></div>
+)
+
+export default Activity
---
+++ src/templates/event.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Event = () => (
+  <div></div>
+)
+
+export default Event
