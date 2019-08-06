3057122ae92c65e3550340a0322fb80b2a769cdc
Mithun Martin Mendez <mithun.martin.215@gmail.com>
2019-08-06 02:09:25 +0200

    usefullinks added

--- src/pages/usefullinks/index.js
+++ src/pages/usefullinks/index.js
@@ -1,28 +1,45 @@
 import React from "react";
 import Layout from "../../components/Layout";
-// import { graphql } from 'gatsby'
+import { graphql } from 'gatsby'
+
+import './usefullinks.sass'
+
+const LinkDisplay = ({ link }) => (
+    <div className="link-container">
+        <a href={`https://${link.title}`} target="__blank" className="text-bold">{link.title}</a>
+        <p className="text-bold">{link.description}</p>
+    </div>
+)
 
 class UsefulLinks extends React.Component {
   render() {
-    console.log(this.props.data)
-    return <Layout></Layout>;
+    const links = this.props.data.allMarkdownRemark.edges
+    return (
+        <Layout>
+            <div className="container contain-wide-text">
+                <div className="spacer-md" />
+                {links.map(link => <LinkDisplay link={link.node.frontmatter} />)}
+            </div>
+        </Layout>
+    )
   }
 }
 
 export default UsefulLinks;
 
-// export const UsefullinksPageQuery = graphql`
-//   query UsefulLinksQuery {
-//     allMarkdownRemark(filter: { frontmatter: { findKey: { e: usefullinks } } }) {
-//       edges {
-//         node {
-//           frontmatter {
-//             title
-//             description
-//           }
-//           excerpt
-//         }
-//       }
-//     }
-//   }
-// `;
+export const UsefullinksPageQuery = graphql`
+  query UsefulLinksQuery {
+    allMarkdownRemark(
+      filter: { frontmatter: { findKey: { eq: "usefullinks" } } }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`;
